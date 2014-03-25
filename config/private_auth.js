323590746f9849419e156587d0575bc576cca63e
theptrk <patrick.tran06@gmail.com>
2014-03-24 20:46:24 -0700

    Authorization configuration barebones added

---
+++ config/private_auth.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: 'xxxxx',
+		'clientSecret' 	: 'xxxxx',
+		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey'  	: 'xxxxx',
+		'consumerSecret': 'xxxxx',
+		'callbackURL' 	: 'http://127.0.0.1:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'xxxxx',
+		'clientSecret' 	: 'xxxxx',
+		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+	},
+
+	'linkedinAuth' : {
+		'apiKey'			: 'xxxxx' ,
+		'secretKey'			: 'xxxxx' ,
+		'OAuth User Token' 	: 'xxxxx' ,
+		'OAuth User Secret' : 'xxxxx' ,
+		'callbackURL'		: 'http://127.0.0.1:8080/auth/linkedin/callback'
+	}
+
+};
+
---
+++ config/private_database.js
@@ -0,0 +1,4 @@
+module.exports = {
+	// same database service. I use MongoHQ.com
+	'url'	: 'mongodb://<username>:<password>@troup.mongohq.com:10065/<database_name>'
+}
